f3d52c95e5e979531e2357d8e33049c63be8210e
MoGood <50001536+MoGood@users.noreply.github.com>
2019-07-07 15:25:01 -0700

    final code for grocery list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { GroceryCompont } from './app.grocery';
+import { FormsModule} from '@angular/forms';
+import {APP_BASE_HREF} from '@angular/common';
+//import { HttpModule} from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    GroceryCompont
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+   // HttpModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
+  bootstrap: [GroceryCompont]
 })
 export class AppModule { }
